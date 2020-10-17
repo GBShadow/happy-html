6fbc4bfc7ced01b5aae5c70ca1a53595cc265d7e
Gustavo B. Sombra <gbs.shadow@gmail.com>
2020-10-17 14:07:33 -0300

    page orphanage detail

---
+++ web/public/scripts/page-orphanage.js
@@ -0,0 +1,21 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false
+}
+
+const map = L.map("mapid", options).setView([-23.6810527, -46.7629219], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+L.marker([-23.6810527, -46.7629219], { icon })
+  .addTo(map)
