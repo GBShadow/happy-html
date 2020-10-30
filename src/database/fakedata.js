00e56b47a4769f8f8dfc61483b00344e0625521d
Gustavo B. Sombra <gbs.shadow@gmail.com>
2020-10-30 00:15:29 -0300

    Start backend

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    lat: -23.6810527,
+    lng: -46.7629219,
+    name: "Lar das meninas",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1576883600124-64c5aa68b4bc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1597730945481-f35a5cf39021?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 8h as 18h",
+    open_on_weekends: "1"
+  },
+  {
+    id: 2,
+    lat: -23.6810527,
+    lng: -46.7729219,
+    name: "Lar dos garotos",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1576883600124-64c5aa68b4bc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1597730945481-f35a5cf39021?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 8h as 18h",
+    open_on_weekends: "0"
+  }
+];
