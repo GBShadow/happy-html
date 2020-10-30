57d91996d47f83d285cd9cb7a9de42c20af1fe75
Gustavo B. Sombra <gbs.shadow@gmail.com>
2020-10-30 00:16:10 -0300

    Change path files

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -11,10 +11,28 @@ const map = L.map("mapid", options).setView([-23.6810527, -46.7629219], 15);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-  iconUrl: "../../public/images/map-marker.svg",
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2]
 });
 
 L.marker([-23.6810527, -46.7629219], { icon }).addTo(map);
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  imageContainer.src = image.src;
+
+  button.classList.add("active");
+}
